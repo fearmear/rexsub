0f68e6223ee88ec1157f0189a59f3e3eea541363
Dmitry Negodaev <fearmear@gmail.com>
2016-09-17 18:18:36 +0300

    improve file name similarity check add test task update test case add bin file

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # rexsub
 Automagically **r**enames **ex**ternal **sub**titles by matching subtitle vs video filename.
 
-#### Example
-Running `./index.js ./test` from project directory will output:
-```
-./test/narcos.s02e01.720p.webrip.x264-skgtv.srt > ./test/Narcos 2 - 01.srt
-./test/narcos.s02e02.720p.webrip.x264-skgtv.srt > ./test/Narcos 2 - 02.srt
-./test/narcos.s02e10.720p.webrip.x264-skgtv.srt > ./test/Narcos 2 - 10.srt
-```
-Use `--dry-run` argument to review the changes before commiting them.
+### Installation
+`npm install -g rexsub`
+
+### Usage
+- Run `rexsub ./directory-with-subtitles-and-video-files --dry-run` to review the changes
+- Run `rexsub ./directory-with-subtitles-and-video-files` to apply them
+
+### Test (manual)
+Run `npm run test`
---
+++ src/index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+var argv = require('yargs').argv;
+var glob = require("glob");
+var similar = require('similar');
+var fs = require('fs');
+var path = require('path');
+
+var extensions = require('./extensions.json');
+var directory = argv._[0];
+var fileExtensionRE = /\.\w+$/;
+var filePathRE = /\/.*\//;
+var specialCharsRE = /[!@#\$%\^\&\*\)\(\\[\]+=]+/g;
+var spaceLikeCharsRE =  /[_-]/g;
+var whiteSpaceRE = /[\s\.]+/g;
+
+if (!directory) {
+    throw new Error('No directory specified');
+}
+
+function getSimplifiedFileName(str){
+  return simplifyFileName(str.replace(filePathRE, ''));
+}
+
+function simplifyFileName(str){
+  return str.replace(fileExtensionRE, '').replace(specialCharsRE, '').replace(spaceLikeCharsRE, ' ').replace(whiteSpaceRE, ' ').trim().toLowerCase()
+}
+
+glob(`${directory}/*.*(${extensions.movies.join('|')})`, function (er, movies) {
+    glob(`${directory}/*.*(${extensions.subtitles.join('|')})`, function (er, subtitles) {
+        subtitles.forEach(function(subtitlePath){
+            subtitlePath = path.resolve(subtitlePath);
+            var similarMovies = movies.map(function(moviePath){
+              moviePath = path.resolve(moviePath);
+              return {
+                similarity: similar(getSimplifiedFileName(moviePath), getSimplifiedFileName(subtitlePath)),
+                moviePath: moviePath
+              }
+            }).sort(function(a, b){
+              return a.similarity > b.similarity ? -1 : 1;
+            });
+            if (similarMovies && similarMovies[0] && similarMovies[0].similarity > 0) {
+                var newPath = similarMovies[0].moviePath.replace(fileExtensionRE, subtitlePath.match(fileExtensionRE)[0]);
+                if (!argv.dryRun) {
+                    fs.renameSync(subtitlePath, newPath);
+                }
+                console.log(`${subtitlePath} > ${newPath}`);
+            } else {
+              console.log(`No similar file names found for ${subtitlePath}`);
+            }
+        });
+    });
+});
