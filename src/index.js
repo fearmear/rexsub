ed70988b9bc000e4f6591c0ca39e57cb03a266f9
Dmitry Kozlovsky <dkozlovsky@fitbit.com>
2016-09-30 18:27:47 +0300

    improve similarity check

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 var argv = require('yargs').argv;
 var glob = require("glob");
-var similar = require('similar');
 var fs = require('fs');
 var path = require('path');
 var q = require('q');
@@ -15,66 +14,78 @@ var filePathRE = /\/.*\//;
 var specialCharsRE = /[!@#\$%\^\&\*\)\(\\[\]+=]+/g;
 var spaceLikeCharsRE =  /[_-]/g;
 var whiteSpaceRE = /[\s\.]+/g;
+var seasonEpisodeRE = /s(\d+)(\s+)?e(\d+)/g;
+var leadingZeroesRE = /0+(\d+)/g;
 
 if (!directory) {
     return console.error('No directory specified.');
 }
 
 function getSimplifiedFileName(filePath){
-  return simplifyFileName(getFileName(filePath));
+    return simplifyFileName(getFileName(filePath));
 }
 
 function getFileName(filePath){
-  return filePath.replace(filePathRE, '');
+    return filePath.replace(filePathRE, '');
 }
 
 function simplifyFileName(fileName){
-  return fileName.replace(fileExtensionRE, '').replace(specialCharsRE, '').replace(spaceLikeCharsRE, ' ').replace(whiteSpaceRE, ' ').trim().toLowerCase();
+    return fileName.replace(fileExtensionRE, '').replace(specialCharsRE, '').replace(spaceLikeCharsRE, ' ')
+        .replace(whiteSpaceRE, ' ').replace(seasonEpisodeRE, '$1 $3').replace(leadingZeroesRE, '$1')
+        .trim().toLowerCase();
 }
 
 function collectRenameCommands(){
-  var deferred = q.defer();
-  glob(`${directory}/*.*(${extensions.movies.join('|')})`, function (er, movies) {
-    glob(`${directory}/*.*(${extensions.subtitles.join('|')})`, function (er, subtitles) {
-      var renameCmds = {};
-      subtitles.forEach(function(subtitlePath){
-          subtitlePath = path.resolve(subtitlePath);
-          var similarMovies = movies.map(function(moviePath){
-            moviePath = path.resolve(moviePath);
-            return {
-              similarity: similar(getSimplifiedFileName(moviePath), getSimplifiedFileName(subtitlePath)),
-              moviePath: moviePath
-            }
-          }).sort(function(a, b){
-            return a.similarity > b.similarity ? -1 : 1;
-          });
-          if (similarMovies && similarMovies[0] && similarMovies[0].similarity > 0) {
-              var newPath = similarMovies[0].moviePath.replace(fileExtensionRE, subtitlePath.match(fileExtensionRE)[0]);
-              if (newPath !== subtitlePath) {
-                renameCmds[subtitlePath] = newPath;
-                console.info(`${getFileName(subtitlePath)} > ${getFileName(newPath)}`);
-              }
-          } else if (argv.verbose) {
-             console.info(`No similar video file name found for ${getFileName(subtitlePath)}`);
-          }
-      });
-      deferred.resolve(renameCmds);
+    var deferred = q.defer();
+    glob(`${directory}/*.*(${extensions.movies.join('|')})`, function (er, movies) {
+        glob(`${directory}/*.*(${extensions.subtitles.join('|')})`, function (er, subtitles) {
+            var renameCmds = {};
+            subtitles.forEach(function(subtitlePath){
+                subtitlePath = path.resolve(subtitlePath);
+                var similarMovies = movies.map(function(moviePath){
+                    moviePath = path.resolve(moviePath);
+                    var movieName = getSimplifiedFileName(moviePath);
+                    var subtitleName = getSimplifiedFileName(subtitlePath);
+                    movieName.split(' ').forEach(function(part){
+                        part.replace();
+                    });
+                    var diff = movieName.replace(new RegExp(subtitleName.split(' ').join('|'), 'g'), '').trim();
+                    var diff2 = subtitleName.replace(new RegExp(movieName.split(' ').join('|'), 'g'), '').trim();
+                    var similarity = movieName.length - diff.length + subtitleName.length - diff2.length;
+                    return {
+                        similarity: similarity,
+                        moviePath: moviePath
+                    }
+                }).sort(function(a, b){
+                    return a.similarity > b.similarity ? -1 : 1;
+                });
+                if (similarMovies && similarMovies[0] && similarMovies[0].similarity > 0) {
+                    var newPath = similarMovies[0].moviePath.replace(fileExtensionRE, subtitlePath.match(fileExtensionRE)[0]);
+                    if (newPath !== subtitlePath) {
+                        renameCmds[subtitlePath] = newPath;
+                        console.info(`${getFileName(subtitlePath)} > ${getFileName(newPath)}`);
+                    }
+                } else if (argv.verbose) {
+                    console.info(`No similar video file name found for ${getFileName(subtitlePath)}`);
+                }
+            });
+            deferred.resolve(renameCmds);
+        });
     });
-  });
-  return deferred.promise;
+    return deferred.promise;
 }
 
 collectRenameCommands().then(function(renameCmds){
-  var filesToRename = Object.keys(renameCmds).length;
-  if (filesToRename === 0) {
-    console.info('Found no files to rename.');
-  } else if (!argv.dryRun) {
-    prompt('Type "y" or "yes" to apply the changes: ', function(answer){
-      if (answer.match(/^y(es)?$/i)) {
-        for (var key in renameCmds) {
-          fs.rename(key, renameCmds[key]);
-        }
-      }
-    });
-  }
+    var filesToRename = Object.keys(renameCmds).length;
+    if (filesToRename === 0) {
+        console.info('Found no files to rename.');
+    } else if (!argv.dryRun) {
+        prompt('Type "y" or "yes" to apply the changes: ', function(answer){
+            if (answer.match(/^y(es)?$/i)) {
+                for (var key in renameCmds) {
+                    fs.rename(key, renameCmds[key]);
+                }
+            }
+        });
+    }
 });
--- test/run.sh
+++ test/run.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
-if [[ $(./src/index.js ./test/files --dry-run --verbose) == "\
+
+failed_test_count=0
+
+if [[ $(./src/index.js ./test/mayoiga --dry-run --verbose) != "\
 HorribleSubs_Mayoiga_-_01.ass > [Leopard-Raws] Mayoiga - 01 RAW (WOWOW 1280x720 x264 AAC).ass
-No similar video file name found for HorribleSubs_Mayoiga_-_02.ass
+HorribleSubs_Mayoiga_-_02.ass > [Leopard-Raws] Mayoiga - 02 RAW (WOWOW 1280x720 x264 AAC).ass
 HorribleSubs_Mayoiga_-_03.ass > [Leopard-Raws] Mayoiga - 03 RAW (WOWOW 1280x720 x264 AAC).ass
 HorribleSubs_Mayoiga_-_04.ass > [Leopard-Raws] Mayoiga - 04 RAW (WOWOW 1280x720 x264 AAC).ass
 HorribleSubs_Mayoiga_-_05.ass > [Leopard-Raws] Mayoiga - 05 RAW (WOWOW 1280x720 x264 AAC).ass
 HorribleSubs_Mayoiga_-_06.ass > [Leopard-Raws] Mayoiga - 06 RAW (WOWOW 1280x720 x264 AAC).ass
-No similar video file name found for HorribleSubs_Mayoiga_-_07.ass
-No similar video file name found for HorribleSubs_Mayoiga_-_08.ass
-No similar video file name found for HorribleSubs_Mayoiga_-_09.ass
-HorribleSubs_Mayoiga_-_10.ass > [Leopard-Raws] Mayoiga - 10 RAW (WOWOW 1280x720 x264 AAC).ass
-No similar video file name found for HorribleSubs_Mayoiga_-_11.ass
-No similar video file name found for HorribleSubs_Mayoiga_-_12.ass" ]];
-  then echo "Success!"; exit 0
-  else echo "Failure!"; exit 1
+HorribleSubs_Mayoiga_-_10.ass > [Leopard-Raws] Mayoiga - 10 RAW (WOWOW 1280x720 x264 AAC).ass" ]];
+    then ((failed_test_count++));
+fi
+
+if [[ $(./src/index.js ./test/nacros 2 --dry-run --verbose) != "\
+narcos.s02e01.720p.webrip.x264-skgtv.srt > Narcos 2 - 01.srt
+narcos.s02e02.720p.webrip.x264-skgtv.srt > Narcos 2 - 02.srt
+narcos.s02e10.720p.webrip.x264-skgtv.srt > Narcos 2 - 10.srt" ]];
+    then ((failed_test_count++));
+fi
+
+if [[ failed_test_count -eq 0 ]];
+    then echo "Success!"; exit 0
+    else echo "Failed test(s): ${failed_test_count}"; exit 1
 fi
