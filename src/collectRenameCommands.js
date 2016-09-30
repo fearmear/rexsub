fec9eee94f66f254c78450648798ae85e4d27c4a
Dmitry Negodaev <fearmear@gmail.com>
2016-10-01 00:01:16 +0300

    refactoring

---
+++ src/collectRenameCommands.js
@@ -0,0 +1,34 @@
+var glob = require("glob");
+var fs = require('fs');
+var path = require('path');
+var q = require('q');
+
+var findSimilarPath = require('./findSimilarPath');
+var filePathToFileName = require('./filePathToFileName');
+
+var extensions = require('../conf/extensions.json');
+var fileExtensionRE = /\.\w+$/;
+
+module.exports = function collectRenameCommands(directory){
+    var deferred = q.defer();
+    glob(`${directory}/*.*(${extensions.movies.join('|')})`, function (er, movies) {
+        glob(`${directory}/*.*(${extensions.subtitles.join('|')})`, function (er, subtitles) {
+            var renameCmds = {};
+            subtitles.forEach(function(subtitlePath){
+                subtitlePath = path.resolve(subtitlePath);
+                var similarPath = findSimilarPath(movies, subtitlePath);
+                if (similarPath) {
+                    var newPath = similarPath.replace(fileExtensionRE, subtitlePath.match(fileExtensionRE)[0]);
+                    if (newPath !== subtitlePath) {
+                        renameCmds[subtitlePath] = newPath;
+                        console.info(`${filePathToFileName(subtitlePath)} > ${filePathToFileName(newPath)}`);
+                    }
+                } else if (argv.verbose) {
+                    console.info(`No similar video file name found for ${filePathToFileName(subtitlePath)}`);
+                }
+            });
+            deferred.resolve(renameCmds);
+        });
+    });
+    return deferred.promise;
+}
--- src/index.js
+++ src/index.js
@@ -1,48 +1,17 @@
 #!/usr/bin/env node
 
 var argv = require('yargs').argv;
-var glob = require("glob");
-var fs = require('fs');
-var path = require('path');
-var q = require('q');
 var prompt = require('cli-prompt');
 
-var extensions = require('../conf/extensions.json');
 var directory = argv._[0];
-var fileExtensionRE = /\.\w+$/;
-
-var findSimilarPath = require('./findSimilarPath');
-var filePathToFileName = require('./filePathToFileName');
 
 if (!directory) {
     return console.error('No directory specified.');
 }
 
-function collectRenameCommands(){
-    var deferred = q.defer();
-    glob(`${directory}/*.*(${extensions.movies.join('|')})`, function (er, movies) {
-        glob(`${directory}/*.*(${extensions.subtitles.join('|')})`, function (er, subtitles) {
-            var renameCmds = {};
-            subtitles.forEach(function(subtitlePath){
-                subtitlePath = path.resolve(subtitlePath);
-                var similarPath = findSimilarPath(movies, subtitlePath);
-                if (similarPath) {
-                    var newPath = similarPath.replace(fileExtensionRE, subtitlePath.match(fileExtensionRE)[0]);
-                    if (newPath !== subtitlePath) {
-                        renameCmds[subtitlePath] = newPath;
-                        console.info(`${filePathToFileName(subtitlePath)} > ${filePathToFileName(newPath)}`);
-                    }
-                } else if (argv.verbose) {
-                    console.info(`No similar video file name found for ${filePathToFileName(subtitlePath)}`);
-                }
-            });
-            deferred.resolve(renameCmds);
-        });
-    });
-    return deferred.promise;
-}
+var collectRenameCommands = require('./collectRenameCommands');
 
-collectRenameCommands().then(function(renameCmds){
+collectRenameCommands(directory).then(function(renameCmds){
     var filesToRename = Object.keys(renameCmds).length;
     if (filesToRename === 0) {
         console.info('Found no files to rename.');
