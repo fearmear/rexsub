b3e29f6b7773ea0c7af0d87d5e73018b3bb0d1d0
Dmitry Negodaev <fearmear@gmail.com>
2016-10-21 00:43:56 +0300

    make seasonEpisodeRE ignore letter case 1.0.5

--- src/simplifyFileName.js
+++ src/simplifyFileName.js
@@ -2,7 +2,7 @@ var fileExtensionRE = /\.\w+$/;
 var specialCharsRE = /[!@#\$%\^\&\*\)\(\\[\]+=]+/g;
 var spaceLikeCharsRE =  /[_-]/g;
 var whiteSpaceRE = /[\s\.]+/g;
-var seasonEpisodeRE = /s(\d+)(\s+)?e(\d+)/g;
+var seasonEpisodeRE = /s(\d+)(\s+)?e(\d+)/gi;
 var leadingZeroesRE = /0+(\d+)/g;
 
 module.exports = function simplifyFileName(fileName){
--- test/run.sh
+++ test/run.sh
@@ -20,6 +20,12 @@ narcos.s02e10.720p.webrip.x264-skgtv.srt > Narcos 2 - 10.srt" ]];
     then ((failed_test_count++));
 fi
 
+if [[ $(./src/index.js ./test/digits --dry-run --verbose) != "\
+foo.s02e06.dvdrip.xvid-bla.srt > Foo S02E06 Bar.srt
+foo.s02e16.dvdrip.xvid-bla.srt > Foo S02E16 Bar.srt" ]];
+    then ((failed_test_count++));
+fi
+
 if [[ failed_test_count -eq 0 ]];
     then echo "Success!"; exit 0
     else echo "Failed test(s): ${failed_test_count}"; exit 1
